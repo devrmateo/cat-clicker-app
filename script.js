747d80d672f094525a86346403cb41f4931f913f
Matthew Smith <mateoelcule@gmail.com>
2019-02-15 18:03:20 -0800

    feat: Add cat list and display area
    When a list item is clicked on, the corresponding cat information is displayed in the content area.

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-let clicks = 1;
-
 function clickModule() {
-     const counters = document.querySelectorAll('figcaption');
-     counters.forEach(function(counter) {
-          counter.innerHTML = 'Number of clicks: ' + clicks;
-          clicks += 1;
-     });
+
+  let cats = [
+    {
+      name: 'Kitty',
+      pic: 'https://farm4.staticflickr.com/3914/15171599148_6ef1d09dab_b.jpg">'
+    },
+    {
+      name: 'Snuggles',
+      pic: 'https://farm4.staticflickr.com/3914/15171599148_6ef1d09dab_b.jpg">'
+    },
+    {
+      name: 'Socks',
+      pic: 'https://farm4.staticflickr.com/3914/15171599148_6ef1d09dab_b.jpg">'
+    },
+    {
+      name: 'Mittens',
+      pic: 'https://farm4.staticflickr.com/3914/15171599148_6ef1d09dab_b.jpg">'
+    },
+    {
+      name: 'Bobbles',
+      pic: 'https://farm4.staticflickr.com/3914/15171599148_6ef1d09dab_b.jpg">'
+    }
+  ];
+
+  populateList(cats);
+  populateDisplay(cats);
 }
 
-function runPage() {
-     const catDivs = document.querySelectorAll('.image');
-     catDivs.forEach(function(catDiv) {
-          catDiv.addEventListener('click', clickModule);
-     });
+function populateList(listData) {
+  const list = document.querySelector('.list');
+  const cats = listData.map(listItem => {
+    return `<li class="listItem"><button>${listItem.name}</button></li>`;
+  }).join('');
+  list.innerHTML = cats;
 }
 
-runPage();
+function populateDisplay(displayData) {
+  const displayArea = document.querySelector('.cat');
+  const list = document.querySelector('.list');
+  list.addEventListener('click', (e) => {
+    let filteredCat = displayData.filter((cat) =>
+      cat.name === e.target.innerHTML)[0];
+    let header = document.createElement('h3');
+    header.innerHTML = filteredCat.name;
+    let image = document.createElement('img');
+    image.src = filteredCat.pic;
+    image.alt = filteredCat.name;
+    displayArea.innerHTML = '';
+    displayArea.insertAdjacentElement('afterbegin', header);
+    displayArea.insertAdjacentElement('beforeend', image);
+  });
+}
+
+clickModule();
