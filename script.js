51053bd4c0e30c16dce450cbe5117b7a8cb90cb4
Matthew Smith <mateoelcule@gmail.com>
2020-08-20 16:45:05 -0700

    feat: Add click functionality for multiple cats

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-function populateImage() {
-  const img = document.querySelector('img')
-  img.src = "images/cat.jpg"
+function populateImages() {
+  const img1 = document.querySelector('.one')
+  const img2 = document.querySelector('.two')
+  img1.src = "images/cat.jpg"
+  img2.src = "images/cat2.jpeg"
 }
 
 function registerClick() {
-  populateImage()
-  const img = document.querySelector('img')
-  console.log(img)
+  populateImages()
+  const img1 = document.querySelector('.one')
+  const img2 = document.querySelector('.two')
+  const arr = [img1, img2]
   let count = 0
-  img.addEventListener('click', (function (num) {
-    return function () {
-      num += 1
-      populateCounter(num)
-      return num
-    }
-  })(count))
-
-
+  addListener(arr, count)
 }
 
-function populateCounter(num) {
-  const counter = document.querySelector('.counter')
-  counter.innerHTML = num
+function addListener(images, count) {
+  images.forEach(image => {
+    image.addEventListener('click', (function (num) {
+      return function () {
+        num += 1
+        populateCounter(num, image)
+        return num
+      }
+    })(count))
+  })
+}
+
+function populateCounter(num, image) {
+  const img = image
+  const counter1 = document.querySelector('.counter1')
+  const counter2 = document.querySelector('.counter2')
+
+  if (img.className === 'one' && counter1.firstElementChild) {
+    counter1.firstElementChild.remove()
+  } else if (img.className === 'two' && counter2.firstElementChild) {
+    counter2.firstElementChild.remove()
+  }
+
+  const span = document.createElement("span")
+  const content = document.createTextNode(` ${num}`)
+
+  span.appendChild(content)
+
+  if (img.className === 'one') {
+    counter1.appendChild(span)
+  } else {
+    counter2.appendChild(span)
+  }
 }
 
 (function runApp() {
