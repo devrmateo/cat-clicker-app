2b5f6a69044359b77dae085d2240800b2c2d8d37
Matthew Smith <mateoelcule@gmail.com>
2020-09-10 16:24:27 -0700

    feat: Add functionality for multiple cats

--- script.js
+++ script.js
@@ -1,60 +1,112 @@
-function populateCats() {
-  const img1 = document.querySelector('.one')
-  const img2 = document.querySelector('.two')
-  img1.src = "images/cat.jpg"
-  img2.src = "images/cat2.jpg"
-  const cat1Name = "Bubbles"
-  const cat2Name = "Daisy"
-  const cat1Header = document.querySelector('.header1')
-  const cat2Header = document.querySelector('.header2')
-  cat1Header.innerHTML = cat1Name
-  cat2Header.innerHTML = cat2Name
-}
+function populateCatsList() {
 
-function registerClick() {
-  const img1 = document.querySelector('.one')
-  const img2 = document.querySelector('.two')
-  const arr = [img1, img2]
-  let count = 0
-  addListener(arr, count)
-}
+  const list = document.querySelector('.catList')
+  const catInfo = [
+    cat1 = {
+      name: "Snuggles",
+      urlInsert: "cat1",
+      count: 0,
+    },
+    cat2 = {
+      name: "Snowy",
+      urlInsert: "cat2",
+      count: 0,
+    },
+    cat3 = {
+      name: "Cute",
+      urlInsert: "cat3",
+      count: 0,
+    },
+    cat4 = {
+      name: "Uggy",
+      urlInsert: "cat4",
+      count: 0,
+    },
+    cat5 = {
+      name: "Sleepy",
+      urlInsert: "cat5",
+      count: 0,
+    }
+  ]
 
-function addListener(images, count) {
-  images.forEach(image => {
-    image.addEventListener('click', (function (num) {
+  catInfo.forEach(cat => {
+    const li = document.createElement("li")
+    list.appendChild(li)
+    const a = document.createElement("a")
+    li.className = 'listItem'
+    li.appendChild(a)
+    a.appendChild(document.createTextNode(`${cat.name}\n`))
+    const img = document.createElement("img")
+    img.className = `image`
+    img.src = `images/${cat.urlInsert}.jpg`
+    img.alt = cat.name
+
+
+    //populate image below header
+    a.addEventListener('click', (function (image) {
       return function () {
-        num += 1
-        populateCounter(num, image)
-        return num
+        const h3 = document.querySelector('.header')
+        if (h3.nextElementSibling) {
+          h3.nextElementSibling.remove()
+        }
+        h3.innerHTML = cat.name
+        h3.insertAdjacentElement('afterend', image)
+        registerClick(cat)
+        const counter = document.querySelector('.counter')
+        if (counter.firstElementChild) {
+          counter.firstElementChild.textContent = cat.count === 0 ? '' : cat.count
+        }
       }
-    })(count))
+    })(img))
   })
 }
 
-function populateCounter(num, image) {
-  const img = image
-  const counter1 = document.querySelector('.counter1')
-  const counter2 = document.querySelector('.counter2')
-
-  if (img.className === 'one' && counter1.firstElementChild) {
-    counter1.firstElementChild.remove()
-  } else if (img.className === 'two' && counter2.firstElementChild) {
-    counter2.firstElementChild.remove()
+function registerClick(cat) {
+  const img = document.querySelector('.image')
+  const counter = document.querySelector('.counter')
+  const text = document.createTextNode('Number of clicks: ')
+  if (!counter.firstChild) {
+    counter.appendChild(text)
   }
+
+
+
+
+  counter.firstElementChild && counter.firstElementChild.textContent === ''
+
+
+
+
+  let count = cat.count
+  img.addEventListener('click', function (num) {
+    return function () {
+      num += 1
+      populateCounter(num, cat)
+      return num
+    }
+  }(count))
+}
+
+function populateCounter(num, cat) {
+
+  cat.count = num
+
+  const counter = document.querySelector('.counter')
+
+  if (counter.firstElementChild) {
+    counter.firstElementChild.remove()
+  }
+
 
   const span = document.createElement("span")
-  const content = document.createTextNode(` ${num}`)
-
+  const content = document.createTextNode(` ${cat.count}`)
   span.appendChild(content)
+  counter.appendChild(span)
 
-  if (img.className === 'one') {
-    counter1.appendChild(span)
-  } else {
-    counter2.appendChild(span)
-  }
+
+
 }
 
 (function runApp() {
-  populateCats()
-  registerClick()
+  populateCatsList()
 })()
